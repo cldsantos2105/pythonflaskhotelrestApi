185e1f41b2d192e7d17cd1cdb0555029fbd93ec2
Claudio Santos <csvirgens@icloud.com>
2021-12-10 15:19:04 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# pythonflaskhotelrestApi
+# pythonflaskhotelrestApi
---
+++ resources/hotel.py
@@ -0,0 +1,110 @@
+
+from sqlite3.dbapi2 import connect
+from typing_extensions import Required
+from flask_restful import Resource, reqparse
+from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
+from models.site import SiteModel
+from resources.filtros import normalize_path_params, consulta_sem_cidade, consulta_com_cidade
+import sqlite3
+
+# path /hoteis?cidade=Salvador&estrelas_min=&diarias_max=400
+
+path_params = reqparse.RequestParser()
+path_params.add_argument('cidade', type=str)
+path_params.add_argument('estrelas_min', type=float)
+path_params.add_argument('estrelas_max', type=float)
+path_params.add_argument('diaria_min', type=float)
+path_params.add_argument('diaria_max', type=float)
+path_params.add_argument('limit', type=float)
+path_params.add_argument('offset', type=float)
+
+class Hoteis(Resource):
+    def get(self):
+        connection = sqlite3.connect('banco.db')
+        cursor = connection.cursor()
+        
+        dados = path_params.parse_args()
+        dados_validos = {chave:dados[chave] for chave in dados if dados[chave] is not None}
+        parametros = normalize_path_params(**dados_validos)
+        
+        if not parametros.get('cidade'):
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta_sem_cidade,tupla)
+        else:
+            tupla = tuple([parametros[chave] for chave in parametros])
+            resultado = cursor.execute(consulta_com_cidade,tupla)
+            
+        hoteis = [] 
+        for linha in resultado: 
+            hoteis.append({
+                'hotel_id': linha[0],
+                'nome': linha[1],
+                'estrelas': linha[2],
+                'diaria': linha[3],
+                'cidade': linha[4],
+                'side_id': linha[5]
+            })   
+            
+        #cidade = parametros.get('cidade')
+        return {'Hoteis':hoteis}
+    
+
+class Hotel(Resource):
+    atributos = reqparse.RequestParser()
+    atributos.add_argument('nome', type=str, required=True, help="The field 'nome' cannot be left in blank" )
+    atributos.add_argument('estrelas')
+    atributos.add_argument('diaria')
+    atributos.add_argument('cidade')
+    atributos.add_argument('site_id', type=int, required=True, help="Every Hotel needs to be linked with a site")
+   
+    
+    def get(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            return hotel.json()
+        return {'message': 'Hotel Not Found'}, 404 # not found
+    
+    @jwt_required()    
+    def post(self, hotel_id):
+        if HotelModel.find_hotel(hotel_id):
+            return {"message":"Hotel id '{}' already exists".format(hotel_id)}, 400 # Bad request
+        
+        dados = Hotel.atributos.parse_args()
+        hotel = HotelModel(hotel_id, **dados)
+        
+        if not SiteModel.find_by_id(dados.get('site_id')):
+            return {'message': 'The hotel must be associated to a valid site id.'}, 400 # Bad request
+        
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message':'An Internal error ocurred trying to save hotel'}, 500 # Internal Server Error
+        return hotel.json(), 200
+    
+    @jwt_required()
+    def put(self, hotel_id):
+        
+        dados = Hotel.atributos.parse_args()
+        hotel_encontrado = HotelModel.find_hotel(hotel_id)
+        if hotel_encontrado:
+            hotel_encontrado.update_hotel(**dados)
+            hotel_encontrado.save_hotel()
+            return hotel_encontrado.json(), 200 # OK
+        hotel = HotelModel(hotel_id, **dados)
+        try:
+            hotel.save_hotel()
+        except:
+            return {'message':'An Internal error ocurred trying to save hotel'}, 500 # Internal Server Error
+        return hotel.json(), 201 # created
+        
+    @jwt_required()
+    def delete(self, hotel_id):
+        hotel = HotelModel.find_hotel(hotel_id)
+        if hotel:
+            try:
+                hotel.delete_hotel()
+            except:
+                return {'message':'An Internal error ocurred trying to delete hotel'}, 500 # Internal Server Error
+            return {'message': 'Hotel Deleted'}
+        return {'message': 'Hotel Not Found'}
